862b3c385f381ae028105d377521c4d586504627
Sherry He <xsherryhe@gmail.com>
2023-01-06 17:59:06 -0500

    Added styling for scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,8 +1,9 @@
 import Score from './Score';
+import '../styles/Scoreboard.css';
 
 export default function Scoreboard({ score = 0, best = 0 }) {
   return (
-    <div>
+    <div className="scoreboard">
       <Score type="current" score={score} />
       <Score type="best" score={best} />
     </div>
