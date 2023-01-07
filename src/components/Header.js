05f42fa008f51efadc73ffdeca6826819bc9a3ed
Sherry He <xsherryhe@gmail.com>
2023-01-07 15:30:29 -0500

    Completed responsive sizing styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,12 @@ import { faRocket } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
   return (
     <header>
-      <FontAwesomeIcon icon={faRocket} />
+      <FontAwesomeIcon className="wide" icon={faRocket} />
+      <div className="narrow">
+        {[...new Array(4)].map((_, i) => (
+          <FontAwesomeIcon key={i} icon={faRocket} />
+        ))}
+      </div>
       <div>
         <h1>Welcome to the Space Donut Shop!</h1>
         <h2>Click on all the donuts.</h2>
@@ -14,7 +19,7 @@ export default function Header() {
           Don't click the same donut twice, or you'll be abducted by aliens!
         </h2>
       </div>
-      <FontAwesomeIcon icon={faRocket} />
+      <FontAwesomeIcon className="wide" icon={faRocket} />
     </header>
   );
 }
